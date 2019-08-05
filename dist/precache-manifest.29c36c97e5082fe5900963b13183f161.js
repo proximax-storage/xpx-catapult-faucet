aa2cdf583fcdaa8e0e6a12a2f14a880f26b326d5
Slackve <eleazartovar123@gmail.com>
2019-08-05 19:43:21 -0400

    new release 0.0.3

---
+++ dist/precache-manifest.29c36c97e5082fe5900963b13183f161.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f807f84b35392f1a9c47",
+    "url": "/js/chunk-vendors.aa407ccc.js"
+  },
+  {
+    "revision": "9eafe3f919eafc9ddb76",
+    "url": "/js/app.b845a277.js"
+  },
+  {
+    "revision": "b2bec0e1e238961bff04",
+    "url": "/js/FaucetGet-vue.6eb7dc99.js"
+  },
+  {
+    "revision": "38babf7ac0e5182fe1d4a411c0875a2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17bb0a03402f1eade48c72edfd2c0e60",
+    "url": "/img/logo-proximax-sirius-faucet_new.17bb0a03.svg"
+  },
+  {
+    "revision": "df9958eb1315357a2476ea02c2e78da6",
+    "url": "/img/icon-wallet-name-red-16h-proximax-sirius-faucet.df9958eb.svg"
+  },
+  {
+    "revision": "9eafe3f919eafc9ddb76",
+    "url": "/css/app.87008a11.css"
+  },
+  {
+    "revision": "b2bec0e1e238961bff04",
+    "url": "/css/FaucetGet-vue.200292df.css"
+  }
+];
