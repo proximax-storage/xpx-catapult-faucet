8fe4be386da52c0bc190a48c574cb054e6ef3b2f
Roimer Peraza <roimerj63@gmail.com>
2020-07-19 14:22:26 -0400

    add multi mosaics support UI

--- frontend/src/views/Faucet.vue
+++ frontend/src/views/Faucet.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="mt-12">
+  <v-container class="mt-8">
     <!-- Title, subtitle -->
     <v-row>
       <v-col cols="12" md="11" class="text-center mx-auto">
@@ -13,89 +13,85 @@
       </v-col>
     </v-row>
 
-    <div class="fauce-container">
-      <div class="container-fluid">
-        <v-form v-model="isValidForm" ref="form">
-          <v-row>
-            <!-- Mosaics Box -->
-            <v-col cols="12" sm="7" md="8" class="mx-auto mt-5" v-if="mosaicsList.length > 0">
-              <div class="d-flex flex-wrap justify-center">
-                <v-card v-ripple v-for="(item, i) in listMosaic" :key="i" outlined tile>
-                  <div
-                    @click="mosaicSelected = item.name"
-                    v-ripple
-                    class="pa-4 cursor-p text-center"
-                    :class="mosaicSelected === item.name ? 'primary white--text': ''"
-                  >
-                    <span class="caption font-weight-medium text-body-2" v-html="item.name"></span>
-                    <br />
-                    <span class="caption font-weight-normal text-caption">
-                      Maximium
-                      <b>{{item.max}}</b>
-                    </span>
-                  </div>
-                </v-card>
-              </div>
-            </v-col>
-
-            <!-- Input Text -->
-            <v-col cols="12" sm="10" md="8" class="mx-auto mt-5 pb-0">
-              <v-text-field
-                v-model="address"
-                dense
-                counter
-                outlined
-                rounded
-                maxlength="46"
-                name="address"
-                type="text"
-                placeholder="Enter your testnet address here..."
-                :rules="[configForm.rules.required, configForm.rules.min, configForm.rules.max, validateAddress]"
+    <v-form v-model="isValidForm" ref="form">
+      <v-row>
+        <!-- Mosaics Box -->
+        <v-col cols="12" sm="7" md="8" class="mx-auto mt-5" v-if="mosaicsList.length > 0">
+          <div class="d-flex flex-wrap justify-center">
+            <v-card v-ripple v-for="(item, i) in listMosaic" :key="i" outlined tile>
+              <div
+                @click="mosaicSelected = item.name"
+                v-ripple
+                class="pa-4 cursor-p text-center"
+                :class="mosaicSelected === item.name ? 'primary white--text': ''"
               >
-                <template v-slot:prepend-inner>
-                  <v-img
-                    class="mr-5 mt-2 mb-4"
-                    alt="logo"
-                    height="30"
-                    width="30"
-                    src="@/assets/img/icon-wallet-name-red-16h-proximax-sirius-faucet.svg"
-                  ></v-img>
-                </template>
-              </v-text-field>
-            </v-col>
-
-            <!-- Message Alert -->
-            <v-col cols="11" sm="9" md="7" class="mx-auto pt-0" v-if="showValidate">
-              <v-alert
-                dark
-                rounded
-                dense
-                outlined
-                class="mb-0"
-                border="left"
-                :type="typeMessage"
-              >{{msjValidate}}</v-alert>
-            </v-col>
-
-            <!-- Button Send -->
-            <v-col cols="12" sm="10" md="8" class="mx-auto">
-              <div class="d-flex justify-center">
-                <v-btn
-                  :disabled="sendButton.disabled"
-                  :loading="sendButton.loading"
-                  class="text-transform-none"
-                  color="primary"
-                  rounded
-                  width="180"
-                  height="40"
-                  @click="sendFaucet"
-                >{{sendButton.text}}</v-btn>
+                <span class="caption font-weight-medium text-body-2" v-html="item.name"></span>
+                <br />
+                <span class="caption font-weight-normal text-caption">
+                  Maximium
+                  <b>{{item.max}}</b>
+                </span>
               </div>
-            </v-col>
-          </v-row>
-        </v-form>
-      </div>
-    </div>
+            </v-card>
+          </div>
+        </v-col>
+
+        <!-- Input Text -->
+        <v-col cols="12" sm="10" md="8" class="mx-auto mt-5 pb-0">
+          <v-text-field
+            v-model="address"
+            dense
+            counter
+            outlined
+            rounded
+            maxlength="46"
+            name="address"
+            type="text"
+            placeholder="Enter your testnet address here..."
+            :rules="[configForm.rules.required, configForm.rules.min, configForm.rules.max, validateAddress]"
+          >
+            <template v-slot:prepend-inner>
+              <v-img
+                class="mr-5 mt-2 mb-4"
+                alt="logo"
+                height="30"
+                width="30"
+                src="@/assets/img/icon-wallet-name-red-16h-proximax-sirius-faucet.svg"
+              ></v-img>
+            </template>
+          </v-text-field>
+        </v-col>
+
+        <!-- Message Alert -->
+        <v-col cols="11" sm="9" md="7" class="mx-auto pt-0" v-if="showValidate">
+          <v-alert
+            dark
+            rounded
+            dense
+            outlined
+            class="mb-0"
+            border="left"
+            :type="typeMessage"
+          >{{msjValidate}}</v-alert>
+        </v-col>
+
+        <!-- Button Send -->
+        <v-col cols="12" sm="10" md="8" class="mx-auto">
+          <div class="d-flex justify-center">
+            <v-btn
+              :disabled="sendButton.disabled"
+              :loading="sendButton.loading"
+              class="text-transform-none"
+              color="primary"
+              rounded
+              width="180"
+              height="40"
+              @click="sendFaucet"
+            >{{sendButton.text}}</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-form>
   </v-container>
 </template>
 <script>
